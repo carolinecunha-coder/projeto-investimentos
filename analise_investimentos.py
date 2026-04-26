69dddf585d412c0dad328993536153c55365954f
caroline <caroline.s.cunha@hotmail.com>
2026-04-26 17:20:11 -0300

    destacando a linha azul no grafico

--- analise_investimentos.py
+++ analise_investimentos.py
@@ -2,8 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
 
-inicio = "2022-01-01"
-fim = "2024-01-01"
+inicio = '2025-12-22'
+fim = '2026-03-02'
 
 acao = yf.download("PETR4.SA", start=inicio, end=fim)
 
@@ -24,12 +24,21 @@ dados["IPCA"] = [(1 + ipca_diario) ** i for i in range(len(dados))]
 dados = dados / dados.iloc[0] * 100
 
 plt.figure(figsize=(10,5))
-plt.plot(dados["PETR4"], label="PETR4")
-plt.plot(dados["CDI"], label="CDI")
-plt.plot(dados["IPCA"], label="IPCA")
+plt.plot(dados["CDI"], label="CDI", color='orange', zorder=1)
+plt.plot(dados["IPCA"], label="IPCA", color='green', zorder=2)
+# O zorder=3 coloca a linha azul por cima das outras
+plt.plot(dados["PETR4"], label="PETR4", color='blue', linewidth=2, zorder=3)
 
 plt.legend()
 plt.grid()
+# Definimos as cores e a ordem (zorder maior fica por cima)
+plt.plot(dados["CDI"], label="CDI", color='orange', zorder=1)
+plt.plot(dados["IPCA"], label="IPCA", color='green', zorder=2)
+plt.plot(dados["PETR4"], label="PETR4", color='blue', zorder=3) # PETR4 por cima de tudo
+
+# 2. Melhora as marcações das datas para tentar mostrar o final de 2025
+plt.gcf().autofmt_xdate()
 plt.show()
+
 
 print(dados.iloc[-1])
